4f61c3e9b02c392c01bf72671d9011c2eca043ca
Shakiran Sathiyanathan <8shak.s@gmail.com>
2024-06-05 23:49:38 -0400

    completed tests

--- tests/test_basics.rs
+++ tests/test_basics.rs
@@ -1,6 +1,35 @@
-use near_sdk::NearToken;
+use std::str::FromStr;
+
+use near_sdk::{AccountId, NearToken};
 use nft_challenger_generator::NFTTokenMetadata;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ChallengeMetaData {
+    // The owner of this NFT Challenge
+    pub owner_id: String,
+    // The name for this challenge.
+    pub name: String,
+    // Free-form description of this challenge.
+    pub description: String,
+    // URL to associated media, preferably to decentralized, content-addressed storage
+    pub media_link: Option<String>,
+    // The id of the reward NFT.
+    pub reward_nft_id: String,
+    // Metadata for the reward token NFT. Only necessary if we mint the nft.
+    pub reward_nft_metadata: NFTTokenMetadata,
+    // Ids of the challenge nfts that are part of this challenge.
+    pub challenge_nft_ids: Vec<String>,
+    // The expiration date of this challenge, expressed as a nano second timestamp.
+    pub expiration_date_in_ns: u64,
+    // Maximum number of winners for this challenge.
+    pub winner_limit: u64,
+    // Number of winners for this challenge.
+    pub winners_count: u64,
+    // Whether the challenge is completed or not.
+    pub challenge_completed: bool,
+}
 
 #[tokio::test]
 async fn test_can_create_challenge() -> Result<(), Box<dyn std::error::Error>> {
@@ -26,7 +55,7 @@ async fn test_can_create_challenge() -> Result<(), Box<dyn std::error::Error>> {
             "reward_nft_metadata": NFTTokenMetadata{
                 title: Some("Test Token".to_string()),
                 description: Some("Test Token".to_string()),
-                media: Some("https://www.creativeuncut.com/gallery-03/art/sa-sonic-05.jpg".to_string()),
+                media: Some("media link".to_string()),
                 copies: Some(1),
                 media_hash: None,
                 expires_at: None,
@@ -51,5 +80,39 @@ async fn test_can_create_challenge() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     assert!(outcome_challenge_exists.json::<bool>().unwrap());
+    println!("GOT HERE 22");
+    let mut s = "test-challenge.".to_string();
+    s.push_str(contract.id().as_str());
+    let metadata_call = sandbox
+        .view(
+            &AccountId::from_str(s.as_str()).unwrap(),
+            "get_challenge_metadata",
+        )
+        .await?;
+    println!("GOT HERE");
+    let metadata: ChallengeMetaData = metadata_call.json().unwrap();
+    assert_eq!(metadata.owner_id, user_account.id().to_string());
+    assert_eq!(metadata.name, "Test Challenge!");
+    assert_eq!(metadata.description, "A test description");
+    assert_eq!(metadata.media_link, Some("A fake media link".to_string()));
+    assert_eq!(metadata.reward_nft_id, "reward-nft-id");
+    assert_eq!(metadata.challenge_nft_ids, vec!["challenge-nft-id"]);
+    assert_eq!(metadata.expiration_date_in_ns, 9007199254740991);
+    assert_eq!(metadata.winner_limit, 100);
+    assert_eq!(metadata.winners_count, 0);
+    assert_eq!(metadata.challenge_completed, false);
+    assert_eq!(
+        metadata.reward_nft_metadata.title,
+        Some("Test Token".to_string())
+    );
+    assert_eq!(
+        metadata.reward_nft_metadata.description,
+        Some("Test Token".to_string())
+    );
+    assert_eq!(
+        metadata.reward_nft_metadata.media,
+        Some("media link".to_string())
+    );
+
     Ok(())
 }
